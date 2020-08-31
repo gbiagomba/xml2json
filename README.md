12e2470dc345a7416855d27ac524946f01c9fe2c
root <root@gbiagomba-sherlock.openstacklocal>
2020-08-31 19:43:07 +0000

    Adding requirements file and other reasources

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # xml2json
-Quick script to convert xml to json
+Quick script to convert xml to json, was originally writen to convert nmap xml files to json (think -oJ)
+
+
+## Installation
+pip install -r requirements.txt
+
+## Usage
+```
+xml2json.py /path/to/nmap/xml/file.xml
+```n
---
+++ xml2json.py
@@ -0,0 +1,11 @@
+#! /bin/env python3
+# https://avleonov.com/2018/03/11/converting-nmap-xml-scan-reports-to-json/
+
+import json
+import xmltodict
+import sys
+
+f = open(sys.argv[1])
+xml_content = f.read()
+f.close()
+print((json.dumps(xmltodict.parse(xml_content), indent=4, sort_keys=True)))
